0cdf78dcd098015603b20bc3998e240e1d7eed51
Lenovo <Lenovo@Aman-Anand>
2022-03-28 13:15:01 +0530

    my first commit

---
+++ org.TestYantra/src/test/java/org/TestYantracom/organization/ClassAmanTest.java
@@ -0,0 +1,20 @@
+package org.TestYantracom.organization;
+
+import org.testng.annotations.Test;
+
+public class ClassAmanTest {
+	
+	@Test
+	public void createAman() {
+		System.out.println("Aman created sucesfully");
+		System.out.println("Aman Updated");
+		System.out.println(" modified");
+		System.out.println("ajhwi");
+		System.out.println("njuhwu");
+		System.out.println("Yougesh marahti");
+		
+	}
+	
+	
+
+}
---
+++ org.TestYantra/src/test/java/org/TestYantracom/organization/CreateContactTest.java
@@ -0,0 +1,16 @@
+package org.TestYantracom.organization;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void contact()
+	{
+		System.out.println("create contact");
+		
+		
+		
+		System.out.println("This is the change");
+	}
+
+}
